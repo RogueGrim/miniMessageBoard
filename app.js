9a888deece9d94c85fc1a8369d790f9a44c69c99
RogueGrim <equan4843@gmail.com>
2026-03-24 16:17:53 +0530

    added post request

--- app.js
+++ app.js
@@ -7,9 +7,22 @@ const app = express()
 app.set('views',path.join(__dirname,'views')) //look for templates in the views directory
 app.set('view engine','ejs')    //sets the view engine to ejs
 
+app.use(express.urlencoded({extended: true}))
+
 //index middleware
 app.get('/',(req, res)=>{
     res.render('index',{title:'Mini-Message Board', messages:messages})
+})
+
+app.get('/new',(req, res)=>[
+    res.render('new')
+])
+
+app.post('/new', (req, res)=>{
+    const message = req.body.text;
+    const name = req.body.user;
+    messages.push({text: message, user: name, added: new Date()})
+    res.redirect('/')
 })
 const PORT = 3000
 
